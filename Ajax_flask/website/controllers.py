c9a110e451006af265fa5d479b1af51510972ddb
Carla Cristina <carldepaula230@gmail.com>
2021-03-09 13:59:56 -0300

    Testando fetch e XmlhttpRequest

---
+++ Ajax_flask/website/controllers.py
@@ -0,0 +1,20 @@
+import random
+
+from flask import render_template, request, jsonify
+from website import website_bp
+
+@website_bp.route('/')
+def index():
+    return render_template('index.html')
+
+@website_bp.route('/sortear')
+def sorteio():
+    inicio = request.args.get('inicio')
+    fim = request.args.get('fim')
+    sorteio = random.randint(int(inicio), int(fim))
+
+    return jsonify(
+        inicio = inicio,
+        fim = fim,
+        sorteio=sorteio
+    )
